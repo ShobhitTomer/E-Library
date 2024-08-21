f746a09b3aa338a8cf538bde2e0e7ef9b1c9ef7a
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-08-21 16:21:23 +0530

    add global error handler

--- Elib-apis-backend/src/app.ts
+++ Elib-apis-backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to elib apis" });
 });
+
+
+//Global error handler
+app.use(globalErrorHandler)
 
 export default app;
