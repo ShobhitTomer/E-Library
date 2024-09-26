55299c3ff107abe205091e85d10f64e26ee3dcc3
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-09-26 21:44:23 +0530

    add user validation

--- Elib-apis-backend/src/users/userController.ts
+++ Elib-apis-backend/src/users/userController.ts
@@ -4,6 +4,8 @@ import createHttpError from "http-errors";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
   //validation
+  //Validation can be much more complex
+  //We also need to do sanitization, library - Express validator
   if (!name || !email || !password) {
     const error = createHttpError(400, "All field are Required");
     return next(error);
