cfabb6d9e1188c4163c8e5126ae7df6ac70f2a6c
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-09-26 22:02:35 +0530

    validation

--- Elib-apis-backend/src/users/userController.ts
+++ Elib-apis-backend/src/users/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -7,9 +8,17 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //Validation can be much more complex
   //We also need to do sanitization, library - Express validator
   if (!name || !email || !password) {
-    const error = createHttpError(400, "All field are Required");
+    const error = createHttpError(400, "All field are required");
     return next(error);
   }
+
+  //Database call to check if there exists the same email or not
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
+    return next(error);
+  }
+
   //process
   //response
   res.json({ message: "User is created" });
