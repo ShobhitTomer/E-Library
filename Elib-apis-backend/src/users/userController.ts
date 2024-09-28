45e2a7c43ea09ed8c7583064721236c45d62147e
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-09-28 23:44:43 +0530

    update: add login endpoint

--- Elib-apis-backend/src/users/userController.ts
+++ Elib-apis-backend/src/users/userController.ts
@@ -59,4 +59,38 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  //DB call to fetch the user
+  let user;
+  try {
+    user = await userModel.findOne({ email });
+  } catch (err) {
+    return next(createHttpError(500, "Error while finding the user"));
+  }
+
+  if (!user) {
+    return next(createHttpError(404, "User not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Email or password incorrect"));
+  }
+
+  //Create access token
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+
+  res.json({ accessToken: token });
+};
+
+export { createUser, loginUser };
