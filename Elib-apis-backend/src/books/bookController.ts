7f2a0ef95838265cbc0c1f844b9937641f7cd689
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-10-03 18:56:12 +0530

    update: completed book creation

--- Elib-apis-backend/src/books/bookController.ts
+++ Elib-apis-backend/src/books/bookController.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import path from "node:path";
+import fs from "node:fs";
 import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
+
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
   //application/pdf - mimetype
   const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
@@ -42,7 +46,24 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 
     console.log("BookuploadResult", bookFileUploadResult);
     console.log("uploadResult", uploadResult);
-    res.json({});
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "66f595e2d2937a5b111a8174",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    //delete temp files
+    try {
+      await fs.promises.unlink(coverImageFilePath);
+      await fs.promises.unlink(bookFilePath);
+    } catch (err) {
+      return next(createHttpError(500, "Error while deleting the local files"));
+    }
+
+    res.status(201).json({ id: newBook._id });
   } catch (err) {
     console.log(err);
     return next(createHttpError(500, "Error while uploading the files"));
