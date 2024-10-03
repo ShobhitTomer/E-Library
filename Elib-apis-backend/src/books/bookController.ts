85b426571a6dea0e13f7e4f77aa7c2a352006da3
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-10-03 18:46:16 +0530

    wip: upload files to cloudinary

--- Elib-apis-backend/src/books/bookController.ts
+++ Elib-apis-backend/src/books/bookController.ts
@@ -1,8 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import path from "node:path";
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({});
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  //application/pdf - mimetype
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const coverImageFileName = files.coverImage[0].filename;
+  //This will generate a absolute path to the file
+  const coverImageFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    coverImageFileName
+  );
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(coverImageFilePath, {
+      filename_override: coverImageFileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+
+    console.log("BookuploadResult", bookFileUploadResult);
+    console.log("uploadResult", uploadResult);
+    res.json({});
+  } catch (err) {
+    console.log(err);
+    return next(createHttpError(500, "Error while uploading the files"));
+  }
 };
 
 export { createBook };
--- Elib-apis-backend/src/books/bookRouter.ts
+++ Elib-apis-backend/src/books/bookRouter.ts
@@ -8,7 +8,7 @@ const bookRouter = express.Router();
 //file store locally -> upload to cloudinary -> delete file from local
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/uploads"),
-  limits: { fileSize: 3e7 }, //30mb
+  limits: { fileSize: 1e7 }, //10mb is the max size allowed by cloudinary hence the limit in multer
 });
 
 //api/books
