488619c55cc4ba69f2f6304a5434b20557a0047c
Shobhit Tomer <shobhittomer0810@gmail.com>
2024-10-11 20:25:01 +0530

    update: add cors support

--- Elib-apis-backend/src/app.ts
+++ Elib-apis-backend/src/app.ts
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./books/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
